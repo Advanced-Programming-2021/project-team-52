65e2a01a3e7b4b1d578290ad2e3d46c5f2fec012
pro-alir <pro.alir.pro@gmail.com>
2021-04-22 22:05:23 +0430

    add Player, User, AI, Deck and Shop model

---
+++ src/AI.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class AI extends Player {
+    private static AI ai;
+    private static Random random;
+    private String nickname;
+
+    private AI() {
+    }
+
+    public static AI getInstance() {
+    }
+
+    private static void makeRandomMainDeck() {
+    }
+
+    private static void makeRandomSideDeck() {
+    }
+
+    private void decide() {
+    }
+}
