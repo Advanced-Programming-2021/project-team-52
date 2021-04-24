4ab6c4abaf921fe292b91be7b8070f96cd4575fe
m-moasa <79235311+m-moasa@users.noreply.github.com>
2021-04-24 08:11:34 +0430

    Update Shop.java

--- src/Shop.java
+++ src/Shop.java
@@ -1,31 +1,41 @@
+package Model.shop;
+
 import java.util.HashMap;
 import java.util.TreeMap;
 
 public class Shop {
     private static Shop shop;
     private PrintBuilder printBuilder;
-    private HashMap<Shop, Integer> items;
+    private static HashMap<String, Integer> items;
 
-    {
+    static {
         items = new HashMap<>();
     }
 
     private Shop() {
     }
 
-
+    public static void addCard(String name, int price){
+        items.put(name ,price);
+    }
     public static Shop getInstance() {
+        if(shop == null){
+            Shop shop = new Shop();
+        }
+        return shop;
     }
 
-    public TreeMap<String, String> getAllShopItems() {
+    //todo : should be tested
+    public TreeMap<String, Integer> getAllShopItems() {
+        return
     }
 
     public int getItemPrize(String name) {
-    }
 
-    private void initializeItem() {
     }
 
     public String toString() {
+        TreeMap treeMap = new TreeMap<String, Integer>(items);
     }
+
 }
