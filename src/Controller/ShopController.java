bf78e75deb100da5f258ebe791e8304db898d161
pro-alir <pro.alir.pro@gmail.com>
2021-04-26 15:41:09 +0430

    Add Shop

--- src/Controller/ShopController.java
+++ src/Controller/ShopController.java
@@ -1,14 +1,31 @@
 package Controller;
 
+import model.*;
+
 public class ShopController {
-    private static Shop shop
+    private static ShopController shop = null;
     private PrintBulider printBulider;
     private printerAndScanner printerAndScanner;
 
-    public ShopController(){}
-    public static Shop getInstance(){}
-    public void run(User user){}
-    public void buy(User user, String cardName){}
-    private boolean checkBeforeTransaction(String cardName, int balance){}
+    private ShopController() {
+        LoginController.
+    }
+
+    public static ShopController getInstance() {
+        if(shop == null)
+            shop = new ShopController();
+        return shop;
+    }
+
+    public void run(User user) {
+    }
+
+    public void buy(User user, String cardName) {
+
+    }
+
+    private boolean checkBeforeTransaction(String cardName, int balance) {
+
+    }
 
 }
