148c040a933413eeb534dbb0d3e1cd88bfd7d3ba
pro-alir <pro.alir.pro@gmail.com>
2021-04-26 15:56:51 +0430

    Add ScoreBoardController

--- src/main/java/controller/ScoreBoardController.java
+++ src/main/java/controller/ScoreBoardController.java
@@ -1,13 +1,63 @@
-package controller;
+package Controller;
+
+import model.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+// say in group : delete run method input (User : user)
 
 public class ScoreBoardController {
-    private ScoreBoard scoreBoard;
+    private static ScoreBoardController scoreBoard = null;
     private PrintBulider printBulider;
     private printerAndScanner printerAndScanner;
+    Collection<model.User> collection = LoginController.users.values();
+    ArrayList<model.User> usersInScoreOrder = new ArrayList<>(collection);
 
-    public ScoreBoardController(){}
-    private static ScoreBoard getInstance(){}
-    public void run(User user)(){}
-    public void sortUserByScore(){}
+    private ScoreBoardController() {
+    }
 
+    public static ScoreBoardController getInstance() {
+        if (scoreBoard == null)
+            scoreBoard = new ScoreBoardController();
+        return scoreBoard;
+    }
+
+    public void run() {
+    }
+
+    private void sortUserByScore() {
+        for (int i = 0; i < usersInScoreOrder.size(); i++) {
+            for (int j = i + 1; j < usersInScoreOrder.size(); j++) {
+                if (usersInScoreOrder.get(i).getScore() > usersInScoreOrder.get(j).getScore())
+                    Collections.swap(usersInScoreOrder, i, j);
+                else if (usersInScoreOrder.get(i).getScore() == usersInScoreOrder.get(j).getScore()) {
+                    if (usersInScoreOrder.get(i).getUsername().toLowerCase()
+                            .compareTo(usersInScoreOrder.get(j).getUsername().toLowerCase()) > 0)
+                        Collections.swap(usersInScoreOrder, i, j);
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        sortUserByScore();
+        StringBuilder response = new StringBuilder();
+        int temporaryRank = 1;
+        for (int i = 0; i < usersInScoreOrder.size(); i++) {
+            if (i != 0) {
+                if (usersInScoreOrder.get(i).getScore() != usersInScoreOrder.get(i - 1).getScore())
+                    temporaryRank = i + 1;
+            }
+            response.append(temporaryRank);
+            response.append("- ");
+            response.append(usersInScoreOrder.get(i).getNickname());
+            response.append(": ");
+            response.append(usersInScoreOrder.get(i).getScore());
+        }
+        return String.valueOf(response);
+    }
 }
