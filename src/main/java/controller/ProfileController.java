b0d4284ae6e69dd2230bf187a2f07d02fb8d64ea
pro-alir <pro.alir.pro@gmail.com>
2021-04-26 15:54:12 +0430

    Add ProfileController

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -1,13 +1,42 @@
-package controller;
+package Controller;
+
+import model.*;
 
 public class ProfileController {
-    private static Profile profile;
+    private static ProfileController profile = null;
     private PrintBulider printBulider;
     private printerAndScanner printerAndScanner;
 
-    private ProfileController(){}
-    public static Profile getinstance(){}
-    public void run(User user){}
-    private static void changeNickname(String newNickname){}
-    private static void changePassword(String newPassword, String oldPassword){}
+    private ProfileController() {
+    }
+
+    public static ProfileController getInstance() {
+        if (profile == null)
+            profile = new ProfileController();
+        return profile;
+    }
+
+    public void run(model.User user) {
+    }
+
+    private static void changeNickname(String newNickname, model.User user) {
+        if (LoginController.nicknames.contains(newNickname))
+            System.out.println("user with nickname " + newNickname + "already exists");
+        else {
+            user.setNickname(newNickname);
+            System.out.println("nickname changed successfully!");
+        }
+    }
+
+
+    private static void changePassword(String newPassword, String oldPassword, model.User user) {
+        if (user.getPassword().equals(oldPassword))
+            System.out.println("current password is invalid");
+        else if (oldPassword.equals(newPassword))
+            System.out.println("please enter a new password");
+        else {
+            user.setPassword(newPassword);
+            System.out.println("password changed successfully!");
+        }
+    }
 }
