3f83c31dd993397709c063755a43adfe6fb99184
pro-alir <pro.alir.pro@gmail.com>
2021-04-26 15:51:52 +0430

    Add User

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,17 +1,16 @@
 package model;
-
-import model.Deck;
-import model.Player;
-
 import java.util.ArrayList;
 import java.util.HashMap;
+
+// need to tell in group : transfer getUserByUsername func from userModel to loginController
+// todo : unit test for all functions
 
 public class User extends Player {
     private String username;
     private String nickname;
     private String password;
-    private int balance;
-    private int score;
+    private int balance = 100000;
+    private int score = 0;
     private HashMap<String, Deck> decks;
     private ArrayList<String> cards;
 
@@ -21,10 +20,16 @@ public class User extends Player {
     }
 
     private User(String username, String password, String nickname) {
-
+        setUsername(username);
+        setPassword(password);
+        setNickname(nickname);
     }
 
     public static void createUser(String username, String password, String nickname) {
+        new User(username, password, nickname);
+        // todo : add user to users arraylists in loginController
+        // todo : handleCreateNewUserProblems such as repetitive username, weak password
+        // todo : check user problems by assignments
     }
 
 
@@ -74,31 +79,81 @@ public class User extends Player {
 
 
     public Deck getDeckByName(String name) {
+        return decks.get(name);
+        // can be null
     }
 
     public ArrayList<Deck> getDeckList() {
+        // check for order
+        ArrayList<Deck> allDecks;
+        if (decks != null) {
+            allDecks = new ArrayList<>(decks.values());
+            return allDecks;
+        }
+        return null;
     }
 
     public Deck getDeck(String name) {
+        return decks.get(name);
+        // can be null
     }
 
 
     public void addDeck(Deck deck) {
+        // is deck with this name already exists
+        decks.put(deck.getName(), deck);
     }
 
     public void deleteDeck(String name) {
+        // is deck with this name exists
+        decks.remove(name);
     }
 
     public void changeBalance(int amount) {
+        this.balance += amount;
+        // amount can be negative
     }
 
     public void changeScore(int amount) {
+        this.score += amount;
+        // amount can be negative
     }
 
     public void addCards(String name) {
+        // is card with this name already exists
+        cards.add(name);
     }
 
+    @Override
     public String toString() {
+        // todo : what do we need for show from User model
+        return "User{" +
+                "username='" + username + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", password='" + password + '\'' +
+                ", balance=" + balance +
+                ", score=" + score +
+                ", decks=" + decks +
+                ", cards=" + cards +
+                '}';
     }
 
+    // can't be tested
+//    public static void main(String[] args) {
+//        User ali = new User("ali", "1234", "MrAli");
+////        createUser("ali", "1234", "MrAli");
+//        Deck deck1 = new Deck("alis deck");
+//        Deck deck2 = new Deck("another alis deck");
+//        ali.addDeck(deck1);
+//        ali.addDeck(deck2);
+//        System.out.println(ali.getDeckByName("alis deck").getName());
+//        System.out.println(ali.getDeckByName("hamchin chizi vojood nadare").getName());
+//
+//        User mamad = new User("mamad", "1234", "MrMamad");
+////        createUser("mamad", "1234", "MrMamad");
+//        Deck deck3 = new Deck("alis deck");
+//        Deck deck4 = new Deck("another mamad deck");
+//        mamad.addDeck(deck3);
+//        mamad.addDeck(deck4);
+//    }
 }
