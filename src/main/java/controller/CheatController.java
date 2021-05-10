c65a42ac9ba5f5a7e3ecc78c94033ff7463938d0
m-moasa <79235311+m-moasa@users.noreply.github.com>
2021-05-10 06:03:17 +0430

    fixed and done

--- src/main/java/controller/CheatController.java
+++ src/main/java/controller/CheatController.java
@@ -2,8 +2,23 @@ package controller;
 
 public class CheatController {
 
-    private static void increaseMoney(User user, int amount){}
-    private static void addCard(User user, String deckName){}
-    public static void increaseLp(int amount){}
-    public static void setDuelWinner(String name){}
+//    private static DuelController duelController;
+//    private static GamePlayController gamePlayController;
+
+    private static void increaseMoney(User user, int amount){
+        int currentMoney = user.getBalance();
+        user.setBalance(currentMoney + amount);
+    }
+    private static void addCard(User user, String cardName){
+        user.addCards(cardName);
+    }
+    
+    //we have them in their own appropriate class
+//    public static void increaseLp(int amount){
+//        gamePlayController.increaseLpWithCheat(amount);
+//    }
+//    public static void setDualWinner(String name){
+//        duelController.setDuelWinnerByCheat(name);
+//    }
+    
 }
