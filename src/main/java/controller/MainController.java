0e459e2895a51599aafa1a70f36491ac0c41447f
m-moasa <79235311+m-moasa@users.noreply.github.com>
2021-05-10 08:14:39 +0430

    finished
    probably should be re-checked

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,11 +1,49 @@
-package controller;
+package Controller;
 
-import com.sun.tools.javac.Main;
+import model.Deck;
+import model.User;
+
+import static model.tools.RegexPatterns.*;
+import static view.PrinterAndScanner.*;
+import java.util.regex.Matcher;
 
 public class MainController {
-    private Main main;
+    private static MainController mainController = null;
 
-    private MainController(){}
-    public Main getInstance(){}
-    public void run(User user){};
+    private MainController() {
+    }
+
+    public static MainController getInstance() {
+        if (mainController == null)
+            mainController = new MainController();
+        return mainController;
+    }
+
+    public void run(User user) {
+        String command = printerAndScanner.scanNextLine().toLowerCase();
+        Matcher matcher;
+        if ((matcher = RegexController.getMatcher(command, menuPattern)) != null) {
+            if (RegexController.hasField(matcher, "enter")) {
+                if (RegexController.hasField(matcher, "duel")) {
+                    DuelController dualController = DuelController.getInstance();
+                    dualController.run(user);
+                } else if (RegexController.hasField(matcher, "deck")) {
+                    DeckController deckController = DeckController.getInstance();
+                    deckController.run(user);
+                } else if (RegexController.hasField(matcher, "scoreboard")) {
+                    ScoreBoardController scoreBoardController = ScoreBoardController.getInstance();
+                    scoreBoardController.run();
+                } else if (RegexController.hasField(matcher, "profile")) {
+                    ProfileController profileController = ProfileController.getInstance();
+                    profileController.run(user);
+                } else if (RegexController.hasField(matcher, "shop")) {
+                    ShopController shopController = ShopController.getInstance();
+                    shopController.run(user);
+                } else {
+                    System.out.println("menu navigation is not possible");
+                }
+            }
+        }
+    }
 }
+
