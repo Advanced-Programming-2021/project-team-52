46f5014879a52330166dfafcda3950f7506a9dbc
m-moasa <mohammad.moasayebi@gmail.com>
2021-07-10 11:20:51 +0430

    nemiresim nnnnnnnnsdgsdfsdfsdfsdfs

--- src/main/java/sample/view/deckManager/DeckManagerViewController.java
+++ src/main/java/sample/view/deckManager/DeckManagerViewController.java
@@ -1,5 +1,8 @@
 package sample.view.deckManager;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXTextArea;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -18,14 +22,26 @@ import java.util.ResourceBundle;
 
 public class DeckManagerViewController implements Initializable {
 
+
     Scene scene;
     Stage stage;
 
     @FXML
     Pane deckManagerPane;
     @FXML
-    JFXTextArea monsterAttributeTextArea,cardToGetSpecialFromTextArea,cardDescriptionTextArea,cardNameTextArea;
-
+    JFXTextArea monsterAttributeTextArea,cardToGetSpecialFromTextArea,cardDescriptionTextArea,cardNameTextArea,
+            cardNameToAddTextArea, cardNameToRemoveTextArea;
+    @FXML
+    JFXRadioButton trapRadiobutton, monsterRadiobutton, spellRadiobutton, limitedRadioButton,
+    halfLimitedRadioButton,unlimitedRadioButton;
+    @FXML
+    Label LVLLabel, ATKLabel, DEFLabel, SpeedLabel, numberOfAvailableCardToAddLabel;
+    @FXML
+    JFXButton submitCardTypeButton, submitCardNameButton, submitCardDescriptionButton, submitCardToGetSpecialFromButton,
+    submitMonsterLVLButton, submitMonsterATKButton, submitMonsterDEFButton, submitCardStatusButton, submitSpeedButton,
+    submitMonsterAttributeButton, add1ToMainButton;
+    @FXML
+    JFXSlider LVLBar, ATKBar, DEFBar, SpeedBar;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -34,6 +50,8 @@ public class DeckManagerViewController implements Initializable {
         cardToGetSpecialFromTextArea.setStyle("-fx-prompt-text-fill: white; -fx-text-fill: white");
         cardDescriptionTextArea.setStyle("-fx-prompt-text-fill: white; -fx-text-fill: white");
         cardNameTextArea.setStyle("-fx-prompt-text-fill: white; -fx-text-fill: white");
+        cardNameToAddTextArea.setStyle("-fx-prompt-text-fill: aqua");
+        cardNameToRemoveTextArea.setStyle("-fx-prompt-text-fill: aqua");
     }
 
     public void backButton(ActionEvent e) throws IOException {
@@ -45,5 +63,50 @@ public class DeckManagerViewController implements Initializable {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void submitCardTypeAndSetting(ActionEvent e){
+        if(monsterRadiobutton.isSelected()){
+
+            submitSpeedButton.setVisible(false);
+            SpeedLabel.setVisible(false);
+            SpeedBar.setVisible(false);
+
+            LVLBar.setVisible(true);
+            ATKBar.setVisible(true);
+            DEFBar.setVisible(true);
+
+            LVLLabel.setVisible(true);
+            ATKLabel.setVisible(true);
+            DEFLabel.setVisible(true);
+
+            submitMonsterLVLButton.setVisible(true);
+            submitMonsterATKButton.setVisible(true);
+            submitMonsterDEFButton.setVisible(true);
+            submitCardStatusButton.setVisible(true);
+
+            monsterAttributeTextArea.setVisible(true);
+            submitMonsterAttributeButton.setVisible(true);
+        } else {
+            LVLBar.setVisible(false);
+            ATKBar.setVisible(false);
+            DEFBar.setVisible(false);
+
+            LVLLabel.setVisible(false);
+            ATKLabel.setVisible(false);
+            DEFLabel.setVisible(false);
+
+            submitMonsterLVLButton.setVisible(false);
+            submitMonsterATKButton.setVisible(false);
+            submitMonsterDEFButton.setVisible(false);
+            submitCardStatusButton.setVisible(false);
+
+            monsterAttributeTextArea.setVisible(false);
+            submitMonsterAttributeButton.setVisible(false);
+
+            submitSpeedButton.setVisible(true);
+            SpeedLabel.setVisible(true);
+            SpeedBar.setVisible(true);
+        }
     }
 }
