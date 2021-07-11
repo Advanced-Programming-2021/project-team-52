842657bf1c397e872eb572b9ffb4571f8dc256b7
m-moasa <mohammad.moasayebi@gmail.com>
2021-07-11 22:03:04 +0430

    بیت المقدس 18

--- src/main/java/sample/view/scoreBoard/ScoreboardViewController.java
+++ src/main/java/sample/view/scoreBoard/ScoreboardViewController.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import sample.controller.ScoreBoardController;
 import sample.model.User;
 import sample.view.UserKeeper;
@@ -17,44 +18,33 @@ import java.util.ResourceBundle;
 
 public class ScoreboardViewController implements Initializable {
 
-    ScoreBoardController scoreBoardController = ScoreBoardController.getInstance();
-
-    @FXML
-    TableView<User> theScoreBoardTableView;
-    @FXML
-    TableColumn<User, String> usernameColumn;
-    @FXML
-    TableColumn<User, Integer> scoreColumn;
-
-    public List<User> scoreBoardList = scoreBoardController.getTopUsers();
-    ObservableList<User> topUsers = FXCollections.observableArrayList();
+//    ScoreBoardController scoreBoardController = ScoreBoardController.getInstance();
+//
+//    @FXML
+//    TableView<User> theScoreBoardTableView;
+//    @FXML
+//    TableColumn<User, String> usernameColumn;
+//    @FXML
+//    TableColumn<User, Integer> scoreColumn;
+//
+//    public List<User> scoreBoardList = scoreBoardController.getTopUsers();
+//    ObservableList<User> topUsers = FXCollections.observableArrayList();
 
 //    TableView<User> scoreBoard;
 
+    @FXML
+    AnchorPane scoreBoardScenePane;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        usernameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("usernameColumn"));
-//        scoreColumn.setCellValueFactory(new PropertyValueFactory<User, Integer>("scoreColumn"));
-//        theScoreBoardTableView.setItems(scoreBoardList);
-//        if(usernameColumn.getCellFactory().equals(UserKeeper.getInstance().getCurrentUser())){
+        scoreBoardScenePane.setStyle("-fx-background-image: url(media/images/backgrounds/scoreBoardWallpaper.jpg); -fx-background-color: black");
+    }
+
+//    public void getObservableList (){
 //
+//        for (int i = 0; i < scoreBoardList.size(); i++) {
+//            if(i < 20)
+//            topUsers.add(scoreBoardList.get(i));
 //        }
-        getObservableList();
-        usernameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("usernameColumn"));
-//        TableColumn<User, String> nicknameColumn =  new TableColumn<>("NickName");
-        usernameColumn =  new TableColumn<>("NickName");
-        usernameColumn.setMaxWidth(200);
-        usernameColumn.setCellValueFactory(new PropertyValueFactory<>("nickname"));
-        theScoreBoardTableView = new TableView<>();
-        theScoreBoardTableView.setItems(topUsers);
-        theScoreBoardTableView.getColumns().add(usernameColumn);
-    }
-
-    public void getObservableList (){
-
-        for (int i = 0; i < scoreBoardList.size(); i++) {
-            if(i < 20)
-            topUsers.add(scoreBoardList.get(i));
-        }
-    }
+//    }
 }
